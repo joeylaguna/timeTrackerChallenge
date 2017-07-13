3aa441a885f4e8142115b35c2de3e0938e58b2f7
jlaguna <elagunaj@gmail.com>
2017-07-12 21:29:48 -0700

    Render data to table from form

--- src/TimeTrackerTable.js
+++ src/TimeTrackerTable.js
@@ -2,20 +2,60 @@ import React, { Component } from 'react';
 import { Table, Form, FormGroup, Button, FormControl, ControlLabel } from 'react-bootstrap';
   
 class TimeTrackerTable extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks: [],
+      taskName: '',
+      timeSpent: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.updateTaskName = this.updateTaskName.bind(this);
+    this.updateTimeSpent = this.updateTimeSpent.bind(this);
+  }
+
+  handleSubmit(e) {
+    console.log('ASDFASDFS');
+    e.preventDefault();
+    let newTask = {};
+    let date = new Date();
+    date = date.toISOString().substring(0, 10);
+    newTask.task_name = this.state.taskName;
+    newTask.timeSpent = this.state.timeSpent;
+    newTask.date = date;
+    let tasks = this.state.tasks;
+    tasks.push(newTask);
+    this.setState({
+      tasks: tasks
+    });
+  }
+
+  updateTaskName(e) {
+    this.setState({
+      taskName: e.target.value
+    });
+  }
+
+  updateTimeSpent(e) {
+    this.setState({
+      timeSpent: e.target.value
+    });
+  }
+
   render() {
     return (
       <div>
-        <Form inline>
-          <FormGroup controlId="formInlineName">
+        <Form inline onSubmit={this.handleSubmit}>
+          <FormGroup  controlId="formInlineName">
             <ControlLabel>Task</ControlLabel>
             {' '}
-            <FormControl type="text" placeholder="Learn Postgres" />
+            <FormControl onChange={this.updateTaskName} type="text" placeholder="Learn Postgres" value={this.state.taskName}/>
           </FormGroup>
           {' '}
           <FormGroup controlId="formInlineEmail">
             <ControlLabel>Time Spent</ControlLabel>
             {' '}
-            <FormControl type="text" placeholder="2 hours" />
+            <FormControl onChange={this.updateTimeSpent} type="text" placeholder="2 hours" value={this.state.timeSpent} />
           </FormGroup>
           {' '}
           <Button type="submit">
@@ -33,7 +73,16 @@ class TimeTrackerTable extends Component {
             </tr>
           </thead>
           <tbody>
-
+            {
+              this.state.tasks.map((item, index) => {
+                return (<tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{item.task_name}</td>
+                  <td>{item.timeSpent}</td>
+                  <td>{item.date}</td>
+                </tr>)
+              })
+            }
           </tbody>
         </Table>
       </div>
